e86df4b0dfba8300c156910435b77b5d3e0bfc82
Jonathan Grunder <contact@jonathan-grunder.tech>
2020-10-08 18:23:26 +0200

    Initiate project

---
+++ MonitoringCS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mobro_monitoring_cs
+{
+    public partial class MonitoringCS : Form
+    {
+        public MonitoringCS()
+        {
+            InitializeComponent();
+        }
+    }
+}
