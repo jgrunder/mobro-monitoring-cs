19266dce46612dd988a8bda3dd7d449e12160905
Jonathan57500 <jonathan.grunder@gmail.com>
2020-10-08 23:36:50 +0200

    Set the initial configuration and setup of the app

--- MonitoringCS.Designer.cs
+++ MonitoringCS.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1024, 600);
+            this.ClientSize = new System.Drawing.Size(800, 600);
             this.ControlBox = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
--- MonitoringCS.cs
+++ MonitoringCS.cs
@@ -1,5 +1,6 @@
 ﻿using CefSharp;
 using CefSharp.WinForms;
+using System.Configuration;
 using System.Windows.Forms;
 
 namespace mobro_monitoring_cs
@@ -7,16 +8,27 @@ namespace mobro_monitoring_cs
     public partial class MonitoringCS : Form
     {
         public ChromiumWebBrowser browser;
+        // Get all parameters from application configuration file
+        private readonly string url = ConfigurationManager.AppSettings.Get("url");
+        private readonly int screenId = int.Parse(ConfigurationManager.AppSettings.Get("screen_id"));
+        private readonly int screenWidth = int.Parse(ConfigurationManager.AppSettings.Get("screen_width"));
+        private readonly int screenHeight = int.Parse(ConfigurationManager.AppSettings.Get("screen_height"));
         public void InitBrowser()
         {
             Cef.Initialize(new CefSettings());
-            browser = new ChromiumWebBrowser("www.google.com");
+            browser = new ChromiumWebBrowser(url);
             Controls.Add(browser);
             browser.Dock = DockStyle.Fill;
         }
         public MonitoringCS()
         {
             InitializeComponent();
+            // Set application size
+            Width = screenWidth;
+            Height = screenHeight;
+            // Set start position
+            StartPosition = FormStartPosition.Manual;
+            Location = Screen.AllScreens[screenId].WorkingArea.Location;
             InitBrowser();
         }
     }
