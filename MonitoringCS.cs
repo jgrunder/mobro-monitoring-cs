99ebc39a8e55e95aa15812904b45b204dc7a508b
Jonathan57500 <jonathan.grunder@gmail.com>
2020-10-08 22:38:24 +0200

    Using program sample

--- MonitoringCS.cs
+++ MonitoringCS.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CefSharp;
+using CefSharp.WinForms;
 using System.Windows.Forms;
 
 namespace mobro_monitoring_cs
 {
     public partial class MonitoringCS : Form
     {
+        public ChromiumWebBrowser browser;
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("www.google.com");
+            Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
         public MonitoringCS()
         {
             InitializeComponent();
+            InitBrowser();
         }
     }
 }
